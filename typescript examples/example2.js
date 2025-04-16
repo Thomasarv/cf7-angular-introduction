f170f391aedddd311dbcebe658d3da85b9c4632b
Th.Arvaniths <thomas99arv@gmail.com>
2025-04-16 20:13:54 +0300

    Initial Commit

---
+++ typescript examples/example2.js
@@ -0,0 +1,4 @@
+var personDescription1 = function (name, city, age) {
+    return "".concat(name, " lives in ").concat(city, ", his age is ").concat(age, ". In 10 years he will be ").concat(age + 10);
+};
+console.log(personDescription1("Kostas", "Athens", 20));
---
+++ typescript examples/example2.ts
@@ -0,0 +1,4 @@
+const personDescription1 = (name: string, city: string , age: number) => 
+    `${name} lives in ${city}, his age is ${age}. In 10 years he will be ${age+10}`;
+
+console.log(personDescription1("Kostas","Athens",20));

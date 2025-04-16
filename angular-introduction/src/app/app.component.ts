1897229fc601ada3eedcd2ddee6d95b9e1f86397
Th.Arvaniths <thomas99arv@gmail.com>
2025-04-16 21:03:34 +0300

    New component person table component

--- angular-introduction/src/app/app.component.ts
+++ angular-introduction/src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PersonTableComponent } from './components/person-table/person-table.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [PersonTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-introduction';
+  name= "Thomas";
+
+  person = {
+    givenName: 'Thomas',
+    surName: 'Arvaniths',
+    age:20,
+    email: 'thomas99arv@gmail.com'
+  }
 }

93c0ee8b4af4abde52073d1298eca7c439e2f34d
Th.Arvaniths <thomas99arv@gmail.com>
2025-04-28 21:01:18 +0300

    New Components Added

--- angular-introduction/src/app/app.component.ts
+++ angular-introduction/src/app/app.component.ts
@@ -1,20 +1,33 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { PersonTableComponent } from './components/person-table/person-table.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { ListGroupMenuComponent } from './components/list-group-menu/list-group-menu.component';
 
 @Component({
   selector: 'app-root',
-  imports: [PersonTableComponent],
+  imports: [RouterOutlet,RouterLink,ListGroupMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   name= "Thomas";
 
-  person = {
-    givenName: 'Thomas',
-    surName: 'Arvaniths',
-    age:20,
-    email: 'thomas99arv@gmail.com'
-  }
+
+  //Step 1: One way binding of data
+  // person  = {
+  //   givenName: 'Thomas',
+  //   surName: 'Arvaniths',
+  //   age:20,
+  //   email: 'thomas99arv@gmail.com'
+  // }
+
+  //Step 3: Component Input
+//   person0: Person = {
+//     givenName: "Christodoulos",
+//     surName: "Fragkoudakis",
+//     age:55,
+//     email: "chfrag@aueb.gr",
+//     address: "Athens,Greece"    
+//  }
+
+ 
 }
---
+++ angular-introduction/src/app/shared/interfaces/person.ts
@@ -0,0 +1,14 @@
+export interface Address {
+    area:string;
+    road: string;
+}
+
+
+export interface Person {
+    givenName: string;
+    surName: string;
+    age: number;
+    email: string;
+    // address:Address;
+    address:string;
+}
